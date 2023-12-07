a6a2036e5c583dd96f7277525354e22aa2b373b6
Michał Nowak <michal@cdbr.pl>
2023-12-07 17:46:59 +0100

    Day 7 finished

--- src/main/kotlin/pl/cdbr/aoc/aoc2023/Day7.kt
+++ src/main/kotlin/pl/cdbr/aoc/aoc2023/Day7.kt
@@ -1,9 +1,6 @@
 package pl.cdbr.aoc.aoc2023
 
 import java.io.File
-import kotlin.math.max
-import kotlin.math.min
-import kotlin.math.sqrt
 import kotlin.system.measureTimeMillis
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
@@ -11,31 +8,31 @@ import kotlin.time.toDuration
 class Day7(filename: String) {
     val hands = parse(File(filename))
 
-    data class Hand(val cards: String, val bid: Long): Comparable<Hand> {
-        val type: HandType by lazy { HandType.match(this) }
+    data class Hand(val cards: String, val bid: Long) {
+        private val type: HandType by lazy { HandType.match(this) }
+        private val typeWithJokers: HandType by lazy { HandType.match(this, withJokers = true) }
 
-        override fun compareTo(other: Hand): Int {
-            val typeDiff = this.type.compareTo(other.type)
+        fun compareWithoutJokers(other: Hand) = compareWithType(other, Hand::type, CARDS_ORDER)
+        fun compareWithJokers(other: Hand) = compareWithType(other, Hand::typeWithJokers, CARDS_ORDER_WITH_JOKERS)
+
+        private fun compareWithType(other: Hand, typeAccessor: Hand.() -> HandType, cardOrder: String): Int {
+            val typeDiff = this.typeAccessor().compareTo(other.typeAccessor())
             return if (typeDiff != 0) {
                 typeDiff
             } else {
-                cards.compareCards(other.cards)
+                cards.compareCards(other.cards, cardOrder)
             }
         }
 
-        private fun String.compareCards(other: String): Int {
-            val thisWeights = this.toCharArray().map { cardsOrder.indexOf(it) }
-            val otherWeights = other.toCharArray().map { cardsOrder.indexOf(it) }
+        private fun String.compareCards(other: String, cardOrder: String): Int {
+            val thisWeights = this.toCharArray().map { cardOrder.indexOf(it) }
+            val otherWeights = other.toCharArray().map { cardOrder.indexOf(it) }
             val toCompare = thisWeights.zip(otherWeights).firstOrNull { (c1, c2) -> c1 != c2 }
             return toCompare?.let { (c1, c2) -> c1.compareTo(c2) } ?: 0
         }
-
-        companion object {
-            const val cardsOrder = "AKQJT98765432"
-        }
     }
 
-    enum class HandType {
+    enum class HandType(vararg counts: Int) {
         FIVE(5),
         FOUR(4),
         FULL(3, 2),
@@ -44,23 +41,33 @@ class Day7(filename: String) {
         PAIR(2),
         NOTHING;
 
-        val cardCounts: IntArray
-
-        constructor(vararg counts: Int) {
-            cardCounts = counts
-        }
+        val cardCounts: IntArray = counts
 
         companion object {
-            fun match(hand: Hand): HandType {
+            fun match(hand: Hand, withJokers: Boolean = false): HandType {
                 val countedHand = hand.cards
                     .groupBy { it }
                     .map { (char, list) -> char to list.size }
-                    .sortedByDescending { (_, count) -> count }
-                    .toMap().values.toList()
+                    .sortedByDescending { (c, count) -> count * 20 - CARDS_ORDER.indexOf(c) }
 
+                val (jokers, other) = countedHand.partition { (c, _) -> c == 'J' }
+
+                val countedValues = if (withJokers && jokers.isNotEmpty() && other.isNotEmpty()) {
+                    val jokerCount = jokers.first().second
+                    val first = other.first().second
+                    val rest = other.drop(1).map(Pair<Char, Int>::second).toTypedArray()
+                    listOf(listOf(first + jokerCount, *rest))
+                } else {
+                    emptyList()
+                } + listOf(countedHand.toMap().values.toList())
+
+                return countedValues.minOfOrNull(Companion::matchValues) ?: NOTHING
+
+            }
+            private fun matchValues(countedValues: List<Int>): HandType {
                 return HandType.values().find { type ->
                     type.cardCounts
-                        .mapIndexed { index, count -> countedHand[index] == count }
+                        .mapIndexed { index, count -> countedValues.getOrElse(index) { 0 } == count }
                         .all { it }
                 } ?: NOTHING
             }
@@ -77,14 +84,20 @@ class Day7(filename: String) {
     }
 
     fun part1() {
-        val sorted = hands.sorted()
-        println(sorted)
+        val sorted = hands.sortedWith(Hand::compareWithoutJokers)
         val winnings = sorted.mapIndexed { index, hand -> hand.bid * (hands.size - index) }
-        println(winnings)
         println(winnings.sum())
     }
 
     fun part2() {
+        val sorted = hands.sortedWith(Hand::compareWithJokers)
+        val winnings = sorted.mapIndexed { index, hand -> hand.bid * (hands.size - index) }
+        println(winnings.sum())
+    }
+
+    companion object {
+        private const val CARDS_ORDER = "AKQJT98765432"
+        private const val CARDS_ORDER_WITH_JOKERS = "AKQT98765432J"
     }
 }
 
@@ -92,8 +105,9 @@ fun main() {
     val runningTime = measureTimeMillis {
         val day = Day7("resources/aoc2023/day7.txt")
 
-        println(day.hands)
+        print("Part 1 solution is ")
         day.part1()
+        print("Part 2 solution is ")
         day.part2()
     }
     println("Elapsed time: ${runningTime.toDuration(DurationUnit.MILLISECONDS)}")

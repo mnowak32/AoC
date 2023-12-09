cd1529acf400080b7bcbe66cc1899c626b1434a0
Michał Nowak <michal@cdbr.pl>
2023-12-09 16:10:50 +0100

    Day 9 finished in <0.5h

--- src/main/kotlin/pl/cdbr/aoc/aoc2023/Day9.kt
+++ src/main/kotlin/pl/cdbr/aoc/aoc2023/Day9.kt
@@ -7,16 +7,47 @@ import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
 class Day9(filename: String) {
-    private val map = parse(File(filename))
+    private val values = parse(File(filename))
     
-    private fun parse(input: File) {
-        
+    private fun parse(input: File): List<List<Int>> {
+        return input.useLines { lines ->
+            lines.map { line -> line.split("\\s+".toRegex()).map(String::toInt) }.toList()
+        }
+    }
+
+    private fun List<Int>.deltas(): List<Int> {
+        val firsts = this.dropLast(1)
+        val seconds = this.drop(1)
+        return firsts.zip(seconds).map { (f, s) -> s - f }
+    }
+
+    private fun List<Int>.allZeros() = this.all { it == 0 }
+
+    private fun List<Int>.extendForward(): Int {
+        val stack = extensions()
+        return stack.map(List<Int>::last).sum()
+    }
+
+    private fun List<Int>.extendBack(): Int {
+        val stack = extensions()
+        return stack.map(List<Int>::first).mapIndexed { index, i -> if (index % 2 == 1) -i else i }.sum()
+    }
+
+    private fun List<Int>.extensions(): List<List<Int>> {
+        val stack = mutableListOf(this)
+        var current = stack.last()
+        do {
+            current = current.deltas().also { stack.add(it) }
+        } while (!current.allZeros())
+        return stack.toList()
     }
 
     fun part1() {
+        values.map { it.extendForward() }.sum().also(::println)
     }
 
     fun part2() {
+        values.map { it.extendBack() }.sum().also(::println)
     }
 
 }
